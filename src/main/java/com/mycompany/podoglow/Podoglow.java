35bea69b9f72a0ec29ca6853b4694ad1c7914a1e
Zuki123456789 <vicenteelizabeth666@gmail.com>
2026-07-06 02:09:05 -0500

    Añadiendo métodos

--- src/main/java/com/mycompany/podoglow/Podoglow.java
+++ src/main/java/com/mycompany/podoglow/Podoglow.java
@@ -1,81 +1,43 @@
 package com.mycompany.podoglow;
 import javax.swing.JOptionPane;
 public class Podoglow {
+    
+    static double ventaTotal;
 
     public static void main(String[] args) {
         final double POD = 70.00, MANICURAT = 20.00, MANICURAG = 40.00, TRAT = 100.00, PEDI = 50.00;
-        String pregunta;
         
         do{
-            double ventaTotal = 0.0, subtotal;
+            ventaTotal = 0.0;
             int opc, cantServ = 0;
         
             JOptionPane.showMessageDialog(null, "Bienvenido al Sistema de Gestión PodoGlow");
         
             do{
-                opc = Integer.parseInt(JOptionPane.showInputDialog(
-                                       """
-                                       --- CATÁLOGO DE SERVICIOS ---
-                                       Seleccione una opción:
-                                       1. Podología S/70.00
-                                       2. Manicura tradicional S/20.00
-                                       3. Manicura gel S/40.00
-                                       4. Tratamiento de Hongos S/100.00
-                                       5. Pedicura S/50.00
-                                       6. FINALIZAR Y COBRAR
-                                       """
-                ));
-
-                if(opc>0 && opc<6){
-                    while(true){
-                        cantServ = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos servicios realizó"));
-
-                        if(cantServ>0)
-                            break;
-
-                        JOptionPane.showMessageDialog(null, "La cantidad de servicios debe ser mayor a 0");
-                    }
-                }
-
+                opc = menu();
+                
+                if(opc < 6)
+                    cantServ = validarInt("¿Cuántos servicios realizó", 1);
+                
                 switch(opc){
                     case 1:
-                        subtotal = POD*cantServ;
-                        ventaTotal += subtotal;
-                        JOptionPane.showMessageDialog(null, "Agregado: S/ " + subtotal +
-                                                            "\nTotal acumulado: S/ " + ventaTotal
-                        );
+                        calcularPrecio(POD, cantServ);
                         break;
 
                     case 2:
-                        subtotal = MANICURAT*cantServ;
-                        ventaTotal += subtotal;
-                        JOptionPane.showMessageDialog(null, "Agregado: S/ " + subtotal +
-                                                            "\nTotal acumulado: S/ " + ventaTotal
-                        );
+                        calcularPrecio(MANICURAT, cantServ);
                         break;
 
                     case 3:
-                        subtotal = MANICURAG*cantServ;
-                        ventaTotal += subtotal;
-                        JOptionPane.showMessageDialog(null, "Agregado: S/ " + subtotal +
-                                                            "\nTotal acumulado: S/ " + ventaTotal
-                        );
+                        calcularPrecio(MANICURAG, cantServ);
                         break;
 
                     case 4:
-                        subtotal = TRAT*cantServ;
-                        ventaTotal += subtotal;
-                        JOptionPane.showMessageDialog(null, "Agregado: S/ " + subtotal +
-                                                            "\nTotal acumulado: S/ " + ventaTotal
-                        );
+                        calcularPrecio(TRAT, cantServ);
                         break;
 
                     case 5:
-                        subtotal = PEDI*cantServ;
-                        ventaTotal += subtotal;
-                        JOptionPane.showMessageDialog(null, "Agregado: S/ " + subtotal +
-                                                            "\nTotal acumulado: S/ " + ventaTotal
-                        );
+                        calcularPrecio(PEDI, cantServ);
                         break;
 
                     case 6:
@@ -93,18 +55,64 @@ public class Podoglow {
                 }
             }while(opc!=6);
             
-            while (true){
-                pregunta = JOptionPane.showInputDialog("¿Desea ingresar consumo de otro usuario?"+
-                                                   "\nSi"+
-                                                   "\nNo"
-                );
-                if (pregunta.equalsIgnoreCase("Si")|| pregunta.equalsIgnoreCase("No"))
-                    break;
-                
-                JOptionPane.showMessageDialog(null, "Opción no válida: solo puede ingresar (SI o NO)");
-            }
+        }while(validarPregunta());
+    }
+  
+    public static int menu(){
+        int opc;
+        while(true){
+            opc = validarInt(
+                    """
+                    --- CATÁLOGO DE SERVICIOS ---
+                    Seleccione una opción:
+                    1. Podología S/70.00
+                    2. Manicura tradicional S/20.00
+                    3. Manicura gel S/40.00
+                    4. Tratamiento de Hongos S/100.00
+                    5. Pedicura S/50.00
+                    6. FINALIZAR Y COBRAR
+                    """, 1
+            );
             
-        }while(pregunta.equalsIgnoreCase("Si"));
+            if(opc<=6) return opc;
+            
+            JOptionPane.showMessageDialog(null, "Error: opción inválida");
+        }
     }
     
+    public static int validarInt(String mensaje, int numMin){
+        int num;
+        while(true){
+            try{
+                num = Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+                
+                if(num>=numMin) return num;
+                
+                JOptionPane.showMessageDialog(null, "Error: el número ingresado debe ser igual o mayor a "+numMin);
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Error: solo debe ingresar números");
+            }
+        }
+    }
+    
+    public static void calcularPrecio(double precio, int cantidad){
+        ventaTotal += (precio*cantidad);
+        JOptionPane.showMessageDialog(null, "Agregado: S/ " + (precio*cantidad) +
+                                            "\nTotal acumulado: S/ " + ventaTotal
+        );
+    }
+    
+    public static boolean validarPregunta(){
+        String pregunta;
+        while (true){
+            pregunta = JOptionPane.showInputDialog("¿Desea ingresar consumo de otro usuario?"+
+                                                   "\nSi"+
+                                                   "\nNo"
+            );
+            if (pregunta.equalsIgnoreCase("Si")|| pregunta.equalsIgnoreCase("No"))
+                return pregunta.equalsIgnoreCase("si");
+                
+            JOptionPane.showMessageDialog(null, "Opción no válida: solo puede ingresar (SI o NO)");
+        }
+    }
 }
